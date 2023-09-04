b6dd3bb7147afb181b7be72188d38b7a13d2701d
hceresetti <henryceresetti0@gmail.com>
2023-09-03 22:57:16 -0300

    src: implements the third subject activity
    Implements the third subject activity assigned, where it was a two numbers multiplying web and local application.

---
+++ app-03-two-numbers-multiplier/App.js
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+import { Text, View, TextInput, Button } from 'react-native';
+
+import { Card } from 'react-native-paper';
+
+import {styles} from './styles'
+
+export default function App() {
+  const [num1, setNum1] = useState(0)
+  const [num2, setNum2] = useState(0)
+  const [result, setResult] = useState(0)
+
+  const multiply = () => {
+    setResult(num1 * num2)
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.p}>Number Multiplier</Text>
+      <Card>
+        <TextInput style={styles.container} placeholder="Input first number" onChangeText={setNum1}/>
+        <TextInput style={styles.container} placeholder="Input second number" onChangeText={setNum2}/>
+        <Button title="Multiply" onPress={multiply}/>
+        <Text style={styles.p}>{result}</Text>
+      </Card>
+    </View>
+  );
+}
---
+++ app-03-two-numbers-multiplier/styles.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 8
+  },
+
+  p: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+});
+
+export {styles}
+
